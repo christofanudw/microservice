6b3f73d0f881ab4269b8b28ee1607170be8c4370
christofanudw <chrstfd98@gmail.com>
2021-08-02 16:55:53 +0200

    Created folder service-user

---
+++ api-gateway/routes/handler/media/getAll.js
@@ -0,0 +1,16 @@
+const apiAdapter = require('../../apiAdapter');
+const { URL_SERVICE_MEDIA } = process.env;
+const api = apiAdapter(URL_SERVICE_MEDIA);
+
+module.exports = async (req,res) => {
+    try {
+        const medias = await api.get('/');
+        return res.json(medias.data);
+    } catch (error) {
+        if(error.code === 'ECONNREFUSED'){
+            return res.status(500).json({ status: 'error', message: 'Service unavailable.' });
+        }
+        const { status, data } = error.response;
+        return res.status(status).json(data)
+    }
+}
