5447a67c435d345aae1f34dd2ba42288134b1d54
christofanudw <chrstfd98@gmail.com>
2021-08-04 21:41:29 +0200

    added register and login API

---
+++ service-user/routes/handler/users/login.js
@@ -0,0 +1,74 @@
+const bcrypt = require('bcrypt');
+const { User } = require('../../../models');
+const Validator = require('fastest-validator');
+const v = new Validator();
+
+module.exports = async (req,res) => {
+    /*
+    *
+    *   Validates login informations provided by user
+    * 
+    */
+    const schema = {
+       email: 'email|empty:false', 
+       password: 'string|min:6',
+    }
+    
+    const validation = v.validate(req.body.password, schema);
+    
+    if(validation.length){
+        return res.status(400).json({
+            status: 'error',
+            message: validation,
+        });
+    };
+    
+    /*
+    *
+    *   Retrieves user model from database
+    * 
+    */
+   const user = await User.findOne({
+       where: {
+           email: req.body.email
+        }
+    });
+    
+    if(!user){
+        res.status(404).json({
+            status: 'error',
+            message: 'User not found.',
+        });
+    };
+    
+    /*
+    *
+    *   Compares provided password w/ user password in database
+    * 
+    */
+   const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+   
+   if(!isValidPassword){
+       res.status(404).json({
+           status: 'error',
+           message: 'User not found.',
+        });
+    }
+    
+    /*
+    *
+    *   Returns user data as response
+    * 
+    */
+    res.json({
+        status: 'success',
+        data: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            avatar: user.avatar,
+            profession: user.profession,
+        }
+    });
+}
---
+++ service-user/routes/handler/users/register.js
@@ -0,0 +1,80 @@
+const bcrypt = require('bcrypt');
+const { User } = require('../../../models');
+const Validator = require('fastest-validator');
+const v = new Validator();
+
+module.exports = async (req,res) => {
+    /*
+    *
+    *   Validates the informations provided by user
+    *
+    */
+    const schema = {
+        name: 'string|empty:false',
+        email: 'email|empty:false',
+        password: 'string|min:6',
+        profession: 'string|optional',
+    }
+
+    const validation = v.validate(req.body, schema);
+
+    if(validation.length){
+        res.status(400).json({
+            status: 'error',
+            message: validation,
+        });
+    };
+
+    /*
+    *
+    *   Checks if email already exists in user database
+    *
+    */
+    const user = await User.findOne({
+        where: { email: req.body.email }
+    });
+
+    if(user){
+        return res.status(409).json({
+            status: 'error',
+            message: 'Email already exists.',
+        });
+    }
+
+    /*
+    *
+    *   Hashes password
+    * 
+    */
+    const password = await bcrypt.hash(req.body.password, 10);
+
+    /*
+    *
+    *   Creates data object
+    * 
+    */
+    const data = {
+        name: req.body.name,
+        email: req.body.email,
+        password,
+        role: 'student',
+        profession: req.body.profession,
+    }
+
+    /*
+    *
+    *   Inserts data object into database though User model
+    *
+    */
+    const createUser = await User.create(data);
+
+    /*
+    *
+    *   Returns success response
+    * 
+    */
+    return res.json({
+        status: 'success',
+        message: 'User added successfully.',
+    });
+}
